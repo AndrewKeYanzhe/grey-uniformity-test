d4d61371a5a53288f41d50febe8d707473fb14bb
Andrew <keyanzhe@gmail.com>
2021-07-03 11:57:27 +0800

    feat: fullscreen when hiding settings panel

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+// default variables
 var lum_8bit = 13
 
+
+
+//generic functions
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.webkitRequestFullscreen) { /* Safari */
+		elem.webkitRequestFullscreen();
+	} else if (elem.msRequestFullscreen) { /* IE11 */
+		elem.msRequestFullscreen();
+	}
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+	if (document.exitFullscreen) {
+		document.exitFullscreen();
+	} else if (document.webkitExitFullscreen) { /* Safari */
+		document.webkitExitFullscreen();
+	} else if (document.msExitFullscreen) { /* IE11 */
+		document.msExitFullscreen();
+	}
+}
+
+
+
+//program specific functions
 function setColor(color){
 	lum_8bit = color
 	document.getElementById("luminance_8bit").textContent = color
@@ -35,12 +68,7 @@ function applyTextValue(){
 	}
 
 	lum_8bit = Math.round(document.getElementById("textValue").value)
-
-
-
 	lum_8bit = Math.min(Math.max(lum_8bit, 0), 255);
-
-
 
 	document.getElementById("textValue").value = lum_8bit
 	if (!isNaN(lum_8bit)){
@@ -54,6 +82,8 @@ function checkKey(e) {
 	e = e || window.event;
 	var colorChange = 0
 
+
+	//arrow keys
 	if (e.keyCode == '38') {
 		var colorChange = 1
 	}
@@ -70,14 +100,20 @@ function checkKey(e) {
 		setColor(newColor)
 	}
 
+
+	//space key
 	if (e.keyCode == '32') {
 
 		var panel = document.getElementById("panel")
-		if (panel.style.display === "none") {
-			panel.style.display = "block";
+
+		if (panel.classList.contains('hide')){
+			closeFullscreen()
 		} else {
-			panel.classList.toggle('hide');
+			openFullscreen()
 		}
+
+		panel.classList.toggle('hide');
+
 	}
 
 
